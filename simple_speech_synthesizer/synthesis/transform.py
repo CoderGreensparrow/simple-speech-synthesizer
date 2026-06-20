f521b9bec4441e8541a84dfd8f0d1fdec644b4d2
CoderGreensparrow <86396838+CoderGreensparrow@users.noreply.github.com>
2026-06-21 00:43:01 +0200

    More synthesis layer stuff + testing code for that layer

--- simple_speech_synthesizer/synthesis/transform.py
+++ simple_speech_synthesizer/synthesis/transform.py
@@ -2,97 +2,121 @@ import pyo
 
 from simple_speech_synthesizer.synthesis import types as this_layer_types
 
-class Synthesizer:
+# This is a bit hacky
+class InitializedEnvelopesInput:
+    def __init__(self, input: this_layer_types.Input):
+        # metaparams
+        self.output_filepath = input.output_filepath
+        self.duration = input.duration
+        # Phoneme synthesis
+        self.Vowel_formant_freqs = [pyo.Linseg(freq) for freq in input.Vowel_formant_freqs]
+        self.Vowel_formant_qs =    [pyo.Linseg(freq) for freq in input.Vowel_formant_qs]
+        self.Vowel_formant_muls =  [pyo.Linseg(freq) for freq in input.Vowel_formant_muls]
+        self.Constriction_formant_freqs = [pyo.Linseg(freq) for freq in input.Constriction_formant_freqs]
+        self.Constriction_formant_qs =    [pyo.Linseg(freq) for freq in input.Constriction_formant_qs]
+        self.Constriction_formant_muls =  [pyo.Linseg(freq) for freq in input.Constriction_formant_muls]
+        self.Voiced_component_mul = pyo.Linseg(input.Voiced_component_mul)
+        self.Voiceless_component_mul = pyo.Linseg(input.Voiceless_component_mul)
+        # Global Envelopes
+        self.Volume = pyo.Linseg(input.Volume)
+        self.F0 = pyo.Linseg(input.F0)
+        self.VocalTiltDelta = pyo.Linseg(input.VocalTiltDelta)
+        self.Tension = pyo.Linseg(input.Tension)
+        # scalar parameters
+        self.F0_freq_sway = input.F0_freq_sway
+        self.F0_freq_FM_jitter = input.F0_freq_FM_jitter
+        self.voice_source_amp_sway = input.voice_source_amp_sway
 
-    def __init__(self):
-        """
-        The Synthesizer audio block.
-        It's an enclosed thing that takes in parameter Envelopes (see base/types),
-        and returns out audio based on them.
-        """
-        pyo_server_kwargs = {
-            "sr": 48000,
-            "nchnls": 2,
-            "buffersize": 256,
-            "duplex": 0,
-            "audio": "offline"
-        }
-        if pyo_server_kwargs is not None:
-            for key, val in pyo_server_kwargs.items():
-                pyo_server_kwargs[key] = val
-        self.s = pyo.Server(**pyo_server_kwargs)
-        self.s.deactivateMidi()
-        self.s.boot()
-        self.s.start()
 
-    def synthesize(self, input: this_layer_types.Input):
-        """
-        The actual synthesis part.
-        :param input: The layer input.
-        :return:
-        """
+def synthesize(input: this_layer_types.Input):
+    """
+    The actual synthesis part.
+    :param input: The layer input.
+    :return:
+    """
 
-        self.s.recordOptions(dur=input.duration, filename=input.output_filepath)
+    pyo_server_kwargs = {
+        "sr": 48000,
+        "nchnls": 2,
+        "buffersize": 256,
+        "duplex": 0,
+        "audio": "offline"
+    }
+    if pyo_server_kwargs is not None:
+        for key, val in pyo_server_kwargs.items():
+            pyo_server_kwargs[key] = val
+    s = pyo.Server(**pyo_server_kwargs)
+    s.deactivateMidi()
+    s.boot()
+    s.recordOptions(dur=input.duration, filename=input.output_filepath)
 
-        # voice source + filter
+    input = InitializedEnvelopesInput(input)
 
-        F0_freq_sway = pyo.ButLP(pyo.BrownNoise(), freq=3, mul=3 * input.F0_freq_sway)
-        F0_freq_FM_jitter = pyo.ButLP(pyo.BrownNoise(), freq=15, mul=0.15 * input.F0_freq_FM_jitter)
-        true_F0 = input.F0 + F0_freq_sway + F0_freq_FM_jitter
+    # voice source + filter
 
-        voice_source_amp_sway = pyo.ButLP(pyo.BrownNoise(), freq=25, mul=0.03 * input.voice_source_amp_sway)
+    F0_freq_sway = pyo.ButLP(pyo.BrownNoise(), freq=3, mul=3 * input.F0_freq_sway)
+    F0_freq_FM_jitter = pyo.ButLP(pyo.BrownNoise(), freq=15, mul=0.15 * input.F0_freq_FM_jitter)
+    true_F0 = input.F0 + F0_freq_sway + F0_freq_FM_jitter
 
-        raw_voice_source = pyo.Blit(freq=true_F0, harms=80, mul=1 + voice_source_amp_sway)
-        spectral_tilted_voice_source = pyo.ButLP(raw_voice_source, 400, mul=1)  # spectral "tilt" (spectral shaping of Blit)
-        high_freq_retention_voice_source = pyo.ButHP(raw_voice_source, 3000, mul=0.01)
-        voice_source = spectral_tilted_voice_source + high_freq_retention_voice_source
+    voice_source_amp_sway = pyo.ButLP(pyo.BrownNoise(), freq=25, mul=0.03 * input.voice_source_amp_sway)
 
-        vowel_f0 = pyo.ButLP(
-                pyo.Reson(voice_source, true_F0, 1, mul=0.6),
-            true_F0 * 1.5)
-        vowel_formants = list()
-        for freq, q, mul in input.Vowel_formant_freqs, input.Vowel_formant_qs, input.Vowel_formant_muls:
-            vowel_formants.append(
-                pyo.Reson(voice_source, freq=freq, q=q, mul=mul)
-            )
+    raw_voice_source = pyo.Blit(freq=true_F0, harms=80, mul=1 + voice_source_amp_sway)
+    spectral_tilted_voice_source = pyo.ButLP(raw_voice_source, 400, mul=1)  # spectral "tilt" (spectral shaping of Blit)
+    high_freq_retention_voice_source = pyo.ButHP(raw_voice_source, 3000, mul=0.01)
+    voice_source = spectral_tilted_voice_source + high_freq_retention_voice_source
 
-        voiced_component = vowel_f0 + sum(vowel_formants)
-        voiced_component *= input.Voiced_component_mul
+    vowel_f0 = pyo.ButLP(
+            pyo.Reson(voice_source, true_F0, 1, mul=0.6),
+        true_F0 * 1.5)
+    vowel_formants = list()
+    for freq, q, mul in input.Vowel_formant_freqs, input.Vowel_formant_qs, input.Vowel_formant_muls:
+        vowel_formants.append(
+            pyo.Reson(voice_source, freq=freq, q=q, mul=mul)
+        )
 
-        # noise source + filter
+    voiced_component = vowel_f0 + sum(vowel_formants)
+    voiced_component *= input.Voiced_component_mul
 
-        noise_source = pyo.Noise()
+    # noise source + filter
 
-        constriction_f0 = pyo.ButLP(pyo.Reson(noise_source, true_F0, 5, mul=0), true_F0 * 1.5)
-        constriction_formants = list()
-        for freq, q, mul in input.Constriction_formant_freqs, input.Constriction_formant_qs, input.Constriction_formant_muls:
-            vowel_formants.append(
-                pyo.Resonx(noise_source, freq=freq, q=q, mul=mul, stages=3)
-            )
+    noise_source = pyo.Noise()
 
-        voiceless_component = constriction_f0 + sum(constriction_formants)
-        voiced_component *= input.Voiceless_component_mul
+    constriction_f0 = pyo.ButLP(pyo.Reson(noise_source, true_F0, 5, mul=0), true_F0 * 1.5)  # TODO I have to parametrize F0 too, or change the architecture
+    constriction_formants = list()
+    for freq, q, mul in input.Constriction_formant_freqs, input.Constriction_formant_qs, input.Constriction_formant_muls:
+        vowel_formants.append(
+            pyo.Resonx(noise_source, freq=freq, q=q, mul=mul, stages=3)
+        )
 
-        # FULL SUM
+    voiceless_component = constriction_f0 + sum(constriction_formants)
+    voiced_component *= input.Voiceless_component_mul
 
-        audio_out = voiced_component + voiceless_component
+    # FULL SUM
 
-        #### RECORD
-        self.s.recstart()
-        for env in input.Vowel_formant_freqs: env.play()
-        for env in input.Vowel_formant_qs: env.play()
-        for env in input.Vowel_formant_muls: env.play()
-        for env in input.Constriction_formant_freqs: env.play()
-        for env in input.Constriction_formant_qs: env.play()
-        for env in input.Constriction_formant_muls: env.play()
-        input.Voiced_component_mul.play()
-        input.Voiceless_component_mul.play()
-        input.Volume.play()
-        input.F0.play()
-        input.VocalTiltDelta.play()
-        input.Tension.play()
-        #### TODO !!!: How to properly exit after recording is done
-        self.s.gui()
+    audio_out = voiced_component + voiceless_component
+    audio_out.out(0)
+    audio_out_2 = audio_out * 1
+    audio_out_2.out(1)
+
+    #### RECORD
+    for env in input.Vowel_formant_freqs: env.play()
+    for env in input.Vowel_formant_qs: env.play()
+    for env in input.Vowel_formant_muls: env.play()
+    for env in input.Constriction_formant_freqs: env.play()
+    for env in input.Constriction_formant_qs: env.play()
+    for env in input.Constriction_formant_muls: env.play()
+    input.Voiced_component_mul.play()
+    input.Voiceless_component_mul.play()
+    input.Volume.play()
+    input.F0.play()
+    input.VocalTiltDelta.play()
+    input.Tension.play()
+    #### TODO !!!: How to properly exit after recording is done
+
+    s.start()
+    s.shutdown()
+
+    return input.output_filepath
 
 
 def transform(input: this_layer_types.Input) -> str:
@@ -101,6 +125,53 @@ def transform(input: this_layer_types.Input) -> str:
     :param input: This layer's Input.
     :return: The filepath to the output audio.
     """
-    s = Synthesizer()
-    s.synthesize(input)
+    synthesize(input)
     return input.output_filepath
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    F0 = 119
+    F1 = 538  # praat value; orig 610
+    F2 = 1779  # praat value; orig 1900
+    F3 = 2751  # praat value
+    # parameters adapted from pyo_playground_e.py
+    i = this_layer_types.Input(
+        output_filepath=r"D:\PycharmProjects\simple-speech-synthesizer\simple_speech_synthesizer\synthesis\testaudio.wav",
+        duration=1.0,
+        Vowel_formant_freqs=[[(0, F1), (1, F1)],
+                             [(0, F2), (1, F2)],
+                             [(0, F3), (1, F3)]],
+        Vowel_formant_qs=[[(0, 6), (1, 6)],
+                          [(0, 8), (1, 8)],
+                          [(0, 12), (1, 12)]],
+        Vowel_formant_muls=[[(0, 0.5), (1, 0.5)],
+                          [(0, 0.4), (1, 0.4)],
+                          [(0, 0.2), (1, 0.2)]],
+        Constriction_formant_freqs=[[(0, F1), (1, F1)],
+                                    [(0, F2), (1, F2)],
+                                    [(0, F3), (1, F3)]],
+        Constriction_formant_qs=[[(0, 30), (1, 30)],
+                                 [(0, 40), (1, 40)],
+                                 [(0, 60), (1, 60)]],
+        Constriction_formant_muls=[[(0, 0.5), (1, 0.5)],
+                                   [(0, 0.2), (1, 0.2)],
+                                   [(0, 0.025), (1, 0.025)]],
+        Voiced_component_mul=[(0, 20), (1, 20)],
+        Voiceless_component_mul=[(0, 0.8), (1, 0.8)],
+        Volume=[(0, 1), (1, 1)],  # TODO have this effect the volume
+        F0=[(0, F0), (1, F0)],
+        F0_freq_sway=1,
+        F0_freq_FM_jitter=1,
+        voice_source_amp_sway=1,
+        VocalTiltDelta=[(0, 1), (1, 1)],
+        Tension=[(0, 1), (1, 1)]
+    )
+    o = transform(i)
--- simple_speech_synthesizer/synthesis/types.py
+++ simple_speech_synthesizer/synthesis/types.py
@@ -1,25 +1,34 @@
+from dataclasses import dataclass
+from collections.abc import Sequence
+
 import pyo
 
 # TODO: Rn every interpolation type is Linseg, this may stay like this
 
+@dataclass(frozen=True)
 class Input:
+    """
+    The Inputs to this layer must be formatted as lists of points, or lists of lists of points, as demanded.
+    The inputs are directly passed to pyo.Linseg (so the input has to be pyo-compatible).
+    Except for scalar inputs.
+    """
     # metaparams
     output_filepath: str
     duration: float
     # Phoneme synthesis
-    Vowel_formant_freqs: tuple[pyo.Linseg, ...]
-    Vowel_formant_qs: tuple[pyo.Linseg, ...]
-    Vowel_formant_muls: tuple[pyo.Linseg, ...]
-    Constriction_formant_freqs: tuple[pyo.Linseg, ...]
-    Constriction_formant_qs: tuple[pyo.Linseg, ...]
-    Constriction_formant_muls: tuple[pyo.Linseg, ...]
-    Voiced_component_mul: pyo.Linseg
-    Voiceless_component_mul: pyo.Linseg
+    Vowel_formant_freqs: Sequence[Sequence[tuple[float, float]]]
+    Vowel_formant_qs: Sequence[Sequence[tuple[float, float]]]
+    Vowel_formant_muls: Sequence[Sequence[tuple[float, float]]]
+    Constriction_formant_freqs: Sequence[Sequence[tuple[float, float]]]
+    Constriction_formant_qs: Sequence[Sequence[tuple[float, float]]]
+    Constriction_formant_muls: Sequence[Sequence[tuple[float, float]]]
+    Voiced_component_mul: Sequence[tuple[float, float]]
+    Voiceless_component_mul: Sequence[tuple[float, float]]
     # Global Envelopes
-    Volume: pyo.Linseg
-    F0: pyo.Linseg
-    VocalTiltDelta: pyo.Linseg
-    Tension: pyo.Linseg
+    Volume: Sequence[tuple[float, float]]
+    F0: Sequence[tuple[float, float]]
+    VocalTiltDelta: Sequence[tuple[float, float]]
+    Tension: Sequence[tuple[float, float]]
     # scalar parameters
     F0_freq_sway: float  # these are percentages from 0 to 1 (unbounded)
     F0_freq_FM_jitter: float
